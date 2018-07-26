2fe2be04e7ec643e11765bb18f5b38bd0300d1be
genericsoma@gmail.com <roman@raincode.com>
2018-07-27 01:14:34 +0200

    DH implementation, key store agent, key timeout process

--- test/crypto_poc_test.exs
+++ test/crypto_poc_test.exs
@@ -1,6 +1,7 @@
 defmodule CryptoPocTest do
   use ExUnit.Case
   doctest CryptoPoc
+  alias CryptoPoc, as: CP
 
   @message """
   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
@@ -11,13 +12,57 @@ defmodule CryptoPocTest do
   non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   """
 
-  test "generate key" do
-    assert bit_size(CryptoPoc.generate_key) == 256
+  def generate_key do
+    {:ok, key} = ExCrypto.generate_aes_key(:aes_256, :bytes)
+    key
   end
-  
+
+  test "generate key" do
+    assert bit_size(generate_key()) == 256
+  end
+
   test "roundtrip" do
-    key = CryptoPoc.generate_key
+    key = generate_key()
     encrypted = CryptoPoc.encrypt(key, @message)
     assert CryptoPoc.decrypt(key, encrypted) == @message
+  end
+
+  test "DH test" do
+    {aS, aP} = CP.dh_pair() # Alice
+    {bS, bP} = CP.dh_pair() # Bob
+    aK = CP.dh_key(bP, aS)
+    bK = CP.dh_key(aP, bS)
+    assert bit_size(aK) == 256
+    assert aK == bK
+  end
+
+  test "register key" do
+    CP.start_link()
+    key = generate_key()
+    id = "user1"
+    CP.register_key(id, key)
+    assert CP.get_key(id) == key
+    CP.shutdown()
+  end
+
+  test "delete key" do
+    CP.start_link()
+    key = generate_key()
+    id = "user1"
+    CP.register_key(id, key)
+    CP.forget_key(id)
+    assert CP.get_key(id) == nil
+    CP.shutdown()
+  end
+
+  test "key timeout" do
+    CP.start_link()
+    key = generate_key()
+    id = "user1"
+    CP.register_key(id, key, 1_000)
+    assert CP.get_key(id) == key
+    Process.sleep(2_000)
+    assert CP.get_key(id) == nil
+    CP.shutdown()
   end
 end
